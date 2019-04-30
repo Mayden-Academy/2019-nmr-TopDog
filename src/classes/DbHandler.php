22909002be50e5dc4bb8355af5c8e6e31cede4f8
HazDuck <peter.e.heyes@gmail.com>
2019-04-30 14:29:16 +0100

    created insertBreed, insertImages and getBreed funcitons

--- src/classes/DbHandler.php
+++ src/classes/DbHandler.php
@@ -4,17 +4,28 @@ class DbHandler
 {
     private $dbConnection;
 
-    public function __constructor($db) {
+    public function __construct($db) {
         $this->dbConnection = $db;
     }
 
-    public function insertBreed () {
-
+    public function insertBreed ($db, $breed_name, $sub_breed) {
+        $query = $db->prepare("INSERT INTO `breed_table` (`breed_name`, `sub_breed`) VALUES (:breed_name,:sub_breed)");
+        $query->bindParam(':breed_name', $breed_name);
+        $query->bindParam(':sub_breed', $sub_breed);
+        return $query->execute();
     }
 
-    public function insertImages () {
-
+    public function insertImages ($db, $breed_id, $url_image) {
+        $query = $db->prepare("INSERT INTO `image_table` (`breed_id`, `url_image`) VALUES (:breed_id,:url_image)");
+        $query->bindParam(':breed_id', $breed_id);
+        $query->bindParam(':sub_breed', $url_image);
+        return $query->execute();
     }
 
+    public function getBreed ($db) {
+        $query= $db->prepare("SELECT `id`, `breed_name`, `sub_breed` FROM `breed_table`");
+        $query->execute();
+        return $query->fetchAll(PDO::FETCH_CLASS);
+    }
 
 }
