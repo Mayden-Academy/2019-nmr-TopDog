3b305d0bdcc11ccbbbd3e07f7b9e75c9f21ac9f2
HazDuck <peter.e.heyes@gmail.com>
2019-04-30 14:05:41 +0100

    set up class and methods

---
+++ src/classes/DbHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+class DbHandler
+{
+    private $dbConnection;
+
+    public function __constructor($db) {
+        $this->dbConnection = $db;
+    }
+
+    public function insertBreed () {
+
+    }
+
+    public function insertImages () {
+
+    }
+
+
+}
