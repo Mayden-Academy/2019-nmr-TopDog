fb00e3df2ed34efb73f83e6ad8b88860466b15b3
Jkapella <jvkapella@gmail.com>
2019-05-01 15:55:37 +0100

    added more styles to top dog div

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 <html lang="en">
 <head>
     <title>Top Dog</title>
+    <link href='styles/normalize.css' rel='stylesheet'>
     <link href="styles/styles.css" rel="stylesheet">
 </head>
 <body>
@@ -21,6 +22,11 @@ require_once __DIR__ . '/vendor/autoload.php';
 </form>
 
 <div class="favouriteDogContainer">
+<!--    GET RID OF THIS BEFORE MERGING-->
+    <div class='best-dog'>
+        <div class='dog-image'></div>
+    </div>
+
     <?php
     ?>
 </div>
@@ -28,6 +34,19 @@ require_once __DIR__ . '/vendor/autoload.php';
 <main class="dogHouse">
     <?php
     ?>
+
+    <div class='dog-holder'>
+
+    </div>
+    <div class='dog-holder'>
+
+    </div>
+    <div class='dog-holder'>
+
+    </div>
+    <div class='dog-holder'>
+
+    </div>
 </main>
 
 </body>
