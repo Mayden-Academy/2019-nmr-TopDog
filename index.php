c5584b61592d592e821b4f751959928078e3c797
fredplatt <fred_platt@hotmail.com>
2019-05-01 13:26:00 +0100

    created template to be filled by php and made nice by css

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<html lang="en">
+<head>
+    <title>Top Dog</title>
+</head>
+<body>
+
+<h1>Top Dog</h1>
+
+<form>
+    <select class="breedDropdown" name="Breeds" title="Breeds">
+        <option>Choose your breed!</option>
+        <?php
+        ?>
+    </select>
+    <input type="submit">
+</form>
+
+<div class="favouriteDogContainer">
+    <?php
+    ?>
+</div>
+
+<main class="dogHouse">
+    <?php
+    ?>
+</main>
+
+</body>
+</html>
