41a6a306dbae9730f3d4273cd974094bbcd79a0d
Knux2 <ben_ashfield@hotmail.com>
2019-05-01 12:03:56 +0100

    Instantiated classes to populate database

--- src/scraper/run.php
+++ src/scraper/run.php
@@ -1,2 +1,20 @@
 <?php
 require_once '../../vendor/autoload.php';
+
+$dbConnection = new TopDog\Classes\DbConnection();
+$ch = new TopDog\scraper\Classes\CurlHandler();
+$request = new TopDog\scraper\Classes\APIGrabber($ch);
+$dataProcessor = new TopDog\scraper\Classes\DataProcessor($request);
+$dbHandler = new TopDog\Classes\DbHandler($dbConnection);
+$arrayBreeds = $dataProcessor->processBreedData();
+foreach ($arrayBreeds as $breed) {
+    $dbHandler->insertBreed($breed['breed_name'], $breed['sub_breed']);
+}
+$breeds = $dbHandler->getBreed();
+$urlRequests = $dataProcessor->createImageUrlWithId($breeds);
+$arrayImages = $dataProcessor->processImageData($urlRequests);
+foreach ($arrayImages as $images) {
+    foreach ($images['urlImages'] as $url) {
+        $dbHandler->insertImages($arrayImages['id'], $url);
+    }
+}
