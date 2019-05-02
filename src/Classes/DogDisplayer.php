f9482629710aa22702daea042b542befd08dc7f4
fredplatt <fred_platt@hotmail.com>
2019-05-02 16:59:34 +0100

    added fav functionality plus tests

--- src/Classes/DogDisplayer.php
+++ src/Classes/DogDisplayer.php
@@ -9,9 +9,9 @@ class DogDisplayer
 
         foreach ($dogs as $dog) {
             $img = '<img src="' . $dog->getUrl() . '" alt="doggy">';
-            $dogId = '<input type="hidden" id=“favDogId" name=“favDogId” value=“' . $dog->getId() . '”>';
+            $dogId = '<input type="hidden" id="favDogId" name="favDogId" value="' . $dog->getId() . '">';
             if ($dog->getIsFav()) {
-                $favDiv = '<div class=“best-dog”><div class=“dog-image”>' . $img . '</div><h3>The Best Doggo!</h3></div>';
+                $favDiv = '<div class="best-dog"><div class="dog-image">' . $img . '</div><h3>The Best Doggo!</h3></div>';
             }
             else $dogDiv .= '<div class="dog-holder"><form action="post"><div class="dog-image">' . $img . '</div>' . $dogId . '<input type="submit" value="Make favourite!"></form></div>';
         }
