7e7151f8e5e56c1fd3525bfcd99114714d809c95
aldofiore28 <aldo.fiore95@gmail.com>
2019-05-02 23:54:47 +0100

    changed indentation to respect PSR2

--- src/Classes/DogDisplayer.php
+++ src/Classes/DogDisplayer.php
@@ -12,8 +12,9 @@ class DogDisplayer
             $dogId = '<input type="hidden" id="favDogId" name="favDogId" value="' . $dog->getId() . '">';
             if ($dog->getIsFav()) {
                 $favDiv = '<div class="best-dog"><div class="dog-image">' . $img . '</div><h3>The Best Doggo!</h3></div>';
+            } else {
+                $dogDiv .= '<div class="dog-holder"><form action="post"><div class="dog-image">' . $img . '</div>' . $dogId . '<input type="submit" value="Make favourite!"></form></div>';
             }
-            else $dogDiv .= '<div class="dog-holder"><form action="post"><div class="dog-image">' . $img . '</div>' . $dogId . '<input type="submit" value="Make favourite!"></form></div>';
         }
         return ['faveDog'=>$favDiv, 'dogs'=>$dogDiv];
     }
