d30b54a1be715b60ad90cf020bcf0c72b079b40c
katestarks <katestarks@me.com>
2019-05-02 13:22:38 +0100

    added DocBlocks

--- src/Classes/FormHandler.php
+++ src/Classes/FormHandler.php
@@ -6,10 +6,18 @@ class FormHandler
 {
     private $formValue;
 
+    /**
+     * Gets the value of the select option from the html form and assigns to formValue property
+     */
     public function assignSelectValue() {
         $this->formValue = $_POST['Breeds'];
     }
 
+    /**
+     * Gets the value of the FormHandler property
+     *
+     * @return int which represents the value of the html dropdown
+     */
     public function getSelectIdValue(){
         return $this->formValue;
     }
