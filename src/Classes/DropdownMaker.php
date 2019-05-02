f39a9c8c400b7c11f5266a5da2ee7e94e2038b0a
rrrichard <richard.rabulan@hotmail.co.uk>
2019-05-02 13:28:21 +0100

    added a dropdown class function

--- src/Classes/DropdownMaker.php
+++ src/Classes/DropdownMaker.php
@@ -1,15 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: academy
- * Date: 2019-05-02
- * Time: 13:04
- */
-
 namespace TopDog\Classes;
-
 
 class DropdownMaker
 {
-
+    public function populateDropdown (array $breeds) : string
+    {
+        $dropDownList = '';
+        if ($breeds == [[]] || $breeds == []) {
+            $dropDownList .= "<option>No Doggies</option>";
+        } else
+            if (count($breeds, true) > 0) {
+                foreach ($breeds as $breed) {
+                    if ($breed['sub_breed'] === '') {
+                        $dropDownList .= "<option value = " . $breed['id'] . ">" . $breed['breed_name'] . "</option>";
+                    } else {
+                        $dropDownList .= "<option value = " . $breed['id'] . ">" . $breed['sub_breed'] . "-" . $breed['breed_name'] . "</option>";
+                    }
+                }
+            }
+        return $dropDownList;
+    }
 }
